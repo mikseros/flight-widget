7aed256823b2f1c1f786753a8c90c29ec8db0bda
mikseros <mmadynski@gmail.com>
2022-07-29 21:34:09 +0200

    Generate the data displayed on the board

--- app.js
+++ app.js
@@ -52,6 +52,10 @@ let fligths = [
     }
 ]
 
+const destinations = ["TOKYO", "FRANKFURT", "DUBAI", "BEIRUT"]
+const remarks = ["ON TIME", "CANCELLED", "DELAYED"]
+let hour = 15
+
 function populateTable() {
     for (const flight of fligths) {
         const tableRow = document.createElement("tr")
@@ -76,3 +80,49 @@ function populateTable() {
 }
 
 populateTable()
+
+function generateRandomLetter() {
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    return alphabet.charAt(Math.floor(Math.random() * alphabet.length))
+}
+
+function generateRandomNumber(maxNumber) {
+    const numbers = "1234567890"
+    if (maxNumber) {
+        const newNumbers = numbers.slice(0, maxNumber + 1)
+        return newNumbers.charAt(Math.floor(Math.random() * newNumbers.length))
+    }
+    return numbers.charAt(Math.floor(Math.random() * numbers.length))
+}
+
+function generateTime() {
+    let displayHour = hour
+
+    if (hour < 24) {
+        hour++
+    }
+    if (hour >= 24) {
+        hour = 1
+        displayHour = hour
+    }
+    if (hour < 10) {
+        displayHour = "0" + hour
+    }
+
+    return displayHour + ":" + generateRandomNumber(5) + generateRandomNumber()
+}
+
+function shuffleUp() {
+    fligths.shift()
+    fligths.push({
+        time: generateTime(),
+        destination: destinations[Math.floor(Math.random() * destinations.length)],
+        flight: generateRandomLetter() + generateRandomLetter() + " " + generateRandomNumber() + generateRandomNumber(),
+        gate: generateRandomLetter() + " " + generateRandomNumber() + " " + generateRandomNumber(),
+        remarks: remarks[Math.floor(Math.random() * remarks.length)]
+    })
+    tableBody.textContent = ""
+    populateTable()
+}
+
+setInterval(shuffleUp(), 2000)
